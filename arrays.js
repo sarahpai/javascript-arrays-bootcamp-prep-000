c5c954916c1760c351ba17d0591240e733cecee3
Sarah Pai <pai.sarah89@gmail.com>
2018-08-23 19:25:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,6 +42,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(2)
-  return array.length;
+  array.slice(0,2)
+  return array;
 }
